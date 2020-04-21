f53770c3f5acf0f657e75d46b92209244746101d
Waltteri <waltteri.metsola@gmail.com>
2020-04-21 13:25:56 +0300

    Started creating usermanagement tab for booking adminpanel. Adding new admins working

--- booking-app/src/components/BookingAdminPage/BookinAdminPage.js
+++ booking-app/src/components/BookingAdminPage/BookinAdminPage.js
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import firebase, { auth, firestore } from '../../firebase'
-import { Tabs, Tab, Typography, Paper, CircularProgress, TextField, Button } from '@material-ui/core';
+import { Tabs, Tab, Typography, Paper, CircularProgress, TextField, Button, Tooltip, ExpansionPanel, ExpansionPanelActions, ExpansionPanelDetails, ExpansionPanelSummary, Chip, Divider } from '@material-ui/core';
 import { Link, Switch, Route, useRouteMatch } from 'react-router-dom'
 import { makeStyles } from '@material-ui/core/styles'
+import clsx from 'clsx';
+
 import AddCircleIcon from '@material-ui/icons/AddCircle'
+import CancelIcon from '@material-ui/icons/Cancel'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+
 import { green } from '@material-ui/core/colors'
 
 const useStyles = makeStyles((theme) => ({
@@ -20,6 +25,48 @@ const useStyles = makeStyles((theme) => ({
         '&:hover': {
             color: green[600],
         }
+    },
+    adminListObject: {
+        display: 'inline',
+        margin: 8
+    },
+    removeAdmin: {
+        color: 'red',
+    },
+    root: {
+        width: '100%',
+    },
+    heading: {
+        fontSize: theme.typography.pxToRem(15),
+    },
+    secondaryHeading: {
+        fontSize: theme.typography.pxToRem(15),
+        color: theme.palette.text.secondary,
+    },
+    icon: {
+        verticalAlign: 'bottom',
+        height: 20,
+        width: 20,
+    },
+    details: {
+        alignItems: 'center',
+    },
+    column: {
+        flexBasis: '33.33%',
+    },
+    helper: {
+        borderLeft: `2px solid ${theme.palette.divider}`,
+        padding: theme.spacing(1, 2),
+    },
+    link: {
+        color: theme.palette.primary.main,
+        textDecoration: 'none',
+        '&:hover': {
+            textDecoration: 'underline',
+        },
+    },
+    errorMessage: {
+        color: 'red'
     }
 }))
 
@@ -35,6 +82,8 @@ const BookingAdminPage = () => {
     const [serviceDescription, setServiceDescription] = useState('')
     const [servicePrice, setServicePrice] = useState('')
     const [serviceTimeLength, setServiceTimeLength] = useState('')
+    const [newAdmin, setNewAdmin] = useState('')
+
     const handleChange = (event, newValue) => {
         setValue(newValue)
     }
@@ -42,24 +91,24 @@ const BookingAdminPage = () => {
     const baseAddress = pagematch.params.id
 
     const fetchData = () => {
-        
+
         firestore.collection(`booker${pagematch.params.id}`).get()
-                .then((response) => {
-                    if (response.empty) {
-                        setError(true)
-                        setLoading(false)
-                    }
-                    response.forEach(doc => {
-                        setBookerObject(doc.data())
-                        setLoading(false)
-                    })
-                })
-                .catch(error => {
-                    console.log(error)
-                    setLoading(false)
+            .then((response) => {
+                if (response.empty) {
                     setError(true)
+                    setLoading(false)
+                }
+                response.forEach(doc => {
+                    setBookerObject(doc.data())
+                    setLoading(false)
                 })
-            }
+            })
+            .catch(error => {
+                console.log(error)
+                setLoading(false)
+                setError(true)
+            })
+    }
     useEffect(() => {
         try {
             setLoading(true)
@@ -86,14 +135,15 @@ const BookingAdminPage = () => {
             console.log(serviceDescription)
             console.log(servicePrice)
             console.log(serviceTimeLength)
-            firestore.collection(`booker${pagematch.params.id}`).doc('baseInformation').update({services: firebase.firestore.FieldValue.arrayUnion(serviceObject)})
-            .then((res) => {
+            firestore.collection(`booker${pagematch.params.id}`).doc('baseInformation').update({ services: firebase.firestore.FieldValue.arrayUnion(serviceObject) })
+                .then((res) => {
                     console.log(res)
                     setServiceName('')
                     setServiceDescription('')
                     setServicePrice('')
                     setServiceTimeLength('')
                     fetchData()
+                    setLoading(false)
                 })
         } catch (error) {
             console.log(error)
@@ -101,6 +151,53 @@ const BookingAdminPage = () => {
             setError(true)
         }
 
+    }
+
+    const addNewAdmin = (e) => {
+        e.preventDefault()
+        try {
+            setError(false)
+            setLoading(true)
+            firestore.collection('userCollection').doc(newAdmin).get()
+            .then(response => {
+                if(response.empty || bookerObject.admins.filter(a => a.email === newAdmin).length > 0){
+                    setLoading(false)
+                    setError(true)
+                } else {
+
+                
+                const adminObject = {
+                    email: response.data().email,
+                    name: response.data().name
+                }
+                firestore.collection(`booker${pagematch.params.id}`).doc('baseInformation').update({ admins: firebase.firestore.FieldValue.arrayUnion(adminObject) })
+                .then(res => {
+                    console.log(res)
+                    setNewAdmin('')
+                    fetchData()
+                    setLoading(false)
+                })
+            }
+            })
+            
+
+            
+            /*
+            firestore.collection(`booker${pagematch.params.id}`).doc('baseInformation').update({ services: firebase.firestore.FieldValue.arrayUnion(serviceObject) })
+                .then((res) => {
+                    console.log(res)
+                    setServiceName('')
+                    setServiceDescription('')
+                    setServicePrice('')
+                    setServiceTimeLength('')
+                    fetchData()
+                })
+                */
+        } catch (error) {
+            console.log(error)
+            setLoading(false)
+            setError(true)
+        }
     }
 
     const getTabContent = (tab) => {
@@ -113,8 +210,8 @@ const BookingAdminPage = () => {
                 return <div>Muutokset</div>
             case 3:
                 return <div>Tilastot</div>
-            case 3:
-                return <div>Käyttäjähallinta</div>
+            case 4:
+                return userControlTab()
             default:
                 return 'Unknown step';
         }
@@ -183,6 +280,73 @@ const BookingAdminPage = () => {
                             </Paper>
                         ))}</div>
                 }
+            </div>
+        )
+    }
+
+    const userControlTab = () => {
+
+        return (
+            <div>
+                <Typography variant='h5'>Sivuston ylläpitäjä</Typography>
+                <Typography>{bookerObject.bookerCreator}</Typography>
+                -----------------------------------------------------
+                <Typography>Lisää ja poista admin käyttäjiä sivulta</Typography>
+                <div>
+                <TextField
+                                id="newadmin"
+                                label="Email"
+                                style={{ margin: 4 }}
+                                helperText="Lisää admin emaililla"
+                                margin="dense"
+                                variant='outlined'
+                                disabled={loading}
+                                value={newAdmin}
+                                onChange={({ target }) => setNewAdmin(target.value)}
+                            />
+                            {loading ? <CircularProgress className={classes.addButton} size={25} /> : <Button onClick={addNewAdmin}><AddCircleIcon className={classes.addButton} /></Button>}
+                            {error ? <div className={classes.errorMessage}>Virhe on sattunut, tarkista osoite ja yritä uudelleen</div> : <em />}
+                            </div>
+                {bookerObject.admins.map(admin => (
+                    <div key={admin.email} >
+                        <div className={classes.root}>
+                            <ExpansionPanel >
+                                <ExpansionPanelSummary
+                                    expandIcon={<ExpandMoreIcon />}
+                                    aria-controls="panel1c-content"
+                                    id="panel1c-header"
+                                >
+                                    <div className={classes.column}>
+                                        <Typography className={classes.heading}>{admin.name}</Typography>
+                                    </div>
+                                    <div className={classes.column}>
+                                        <Typography className={classes.secondaryHeading}>{admin.email}</Typography>
+                                    </div>
+                                </ExpansionPanelSummary>
+                                <ExpansionPanelDetails className={classes.details}>
+                                    <div className={classes.column} />
+                                    <div className={classes.column}>
+                                        {admin.email === bookerObject.bookerCreator ? <Chip label="Owner" onDelete={() => { }} /> : <em/>}
+                                        <Chip label="Admin" onDelete={() => { }} />
+                                    </div>
+                                    <div className={clsx(classes.column, classes.helper)}>
+                                        <Typography variant="caption">
+                                           Lisää tietoa
+                                       <br />
+                                        </Typography>
+                                    </div>
+                                </ExpansionPanelDetails>
+                                <Divider />
+                                <ExpansionPanelActions>
+                                <Tooltip title={`Poista henkilön ${admin.name} adminoikeudet `} arrow><Button size='small' className={classes.removeAdmin}><CancelIcon /></Button></Tooltip>
+                                    <Button size="small" color="primary">
+                                        Save
+          </Button>
+                                </ExpansionPanelActions>
+                            </ExpansionPanel>
+                        </div>
+                    </div>
+                ))}
             </div>
         )
     }
